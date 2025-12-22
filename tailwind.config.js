0c3f68401c4c753fdf86bbf11bfccfc04d8c91b0
fabiandiazmartinez20 <fabian_diaz_m@outlook.com>
2025-12-21 23:01:26 -0600

    actualizacion de estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  darkMode: "class", // ← Esto es lo que faltaba
   theme: {
     extend: {},
   },
